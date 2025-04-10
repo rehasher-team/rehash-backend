81a335a258e3b3e394ba3afe88713668bb1f6e88
divopsor <divopsor@gmail.com>
2025-04-10 19:47:33 +0900

    chore: Initialize spring boot project

---
+++ src/main/java/kr/co/rehash/RehashApplication.java
@@ -0,0 +1,13 @@
+package kr.co.rehash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RehashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RehashApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/co/rehash/RehashApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.rehash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RehashApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
