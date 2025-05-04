a14ca26c8bd68d70ce036b69493288a004049068
정석호 <nodejsdeveloper@kakao.com>
2025-05-04 17:45:46 +0900

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 set -e
 
-echo "DB_URL=$DB_URL" > .env
-echo "DB_USERNAME=$DB_USERNAME" > .env
-echo "DB_PASSWORD=$DB_PASSWORD" > .env
+if [ ! -f .env ]; then
+  echo "Creating .env file..."
+  echo "DB_URL=$DB_URL" > .env
+  echo "DB_USERNAME=$DB_USERNAME" >> .env
+  echo "DB_PASSWORD=$DB_PASSWORD" >> .env
+else
+  echo ".env already exists. Skipping creation."
+fi
 
 java -jar app.jar
